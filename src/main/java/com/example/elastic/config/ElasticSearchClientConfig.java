639c75b773ac71f07917a7d38964969abeccc304
hx76 <947769582@qq.com>
2021-03-22 21:54:52 +0800

    elasticsearch曾删改查

--- src/main/java/com/example/elastic/config/ElasticSearchClientConfig.java
+++ src/main/java/com/example/elastic/config/ElasticSearchClientConfig.java
@@ -11,7 +11,7 @@ public class ElasticSearchClientConfig {
     @Bean
     public RestHighLevelClient restHighLevelClient(){
         RestHighLevelClient client = new RestHighLevelClient(
-                RestClient.builder(new HttpHost("49.232.4.183",9200,"http"))
+                RestClient.builder(new HttpHost("localhost",9200,"http"))
         );
         return client;
 
